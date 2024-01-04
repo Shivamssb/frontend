0a98ceee3dc40a39342e3e1c428cf9cd88ab41e0
Shivamssb <140708184+Shivamssb@users.noreply.github.com>
2024-01-04 18:45:47 +0000

    new conversion updated

---
+++ Basics/otherconversions.js
@@ -0,0 +1,33 @@
+/*
+console.log(2+2)
+console.log(5*5)
+console.log(95-77)
+console.log(50/5)
+console.log(8%3)
+//if string comes first then string data can be showed
+console.log("1"+"2"+7)
+//if number comes first then there will be arithmetic operation then string value showed
+console.log(1+7+"2"+7)
+//Boolean value 
+console.log(+true)
+console.log(+"")
+//others
+let sum1 ,sum2 ,sum3 
+sum1 = sum2 = sum3 = 5+5
+console.log(sum3)
+
+*/
+//Data Types Conversion (Boolean : true or false)
+//1 number to number
+console.log(5>4) 
+console.log(15>=4) 
+console.log(5<=4) 
+//2 string to number 
+console.log("5" >= 4) 
+console.log("15" != 4) 
+console.log(7 == "6") 
+
+//== only number comparision but === strict comparision it also compare the data type also
+console.log(5==4) 
+console.log(5==5) 
+console.log("7"===7) 
