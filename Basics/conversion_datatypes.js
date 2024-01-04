cc0a63b479c71d38b7bb3ae5972991099ae21f51
Shivamssb <140708184+Shivamssb@users.noreply.github.com>
2024-01-04 17:47:05 +0000

    updateds

---
+++ Basics/conversion_datatypes.js
@@ -0,0 +1,26 @@
+//Number Conversion
+let num = "24"
+console.log(num)
+console.log(typeof num)
+let newValue = Number(num)
+console.log(typeof newValue)
+/*
+let num = "24abc" or true or false or null or undefined
+console.log(num)
+console.log(typeof num)
+let newValue = Number(num)
+console.log(typeof newValue)
+*/ 
+//Boolean
+let nums = true
+console.log(num)
+console.log(typeof nums)
+let newValues = Boolean(nums)
+console.log(typeof newValues)
+
+//String
+let numb = 24
+console.log(numb)
+console.log(typeof numb)
+let newValueb = String(num)
+console.log(typeof newValueb)
---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+const num = 45
+console.log(num)
+var age = 75
+
+let names = "Shivam"
+console.log(names)
+names="Arjun"
+age = 70
+console.log(age)
+const numberId = 56
+var nameId = "bal Hanuman"
+let cityId = "Indore"
+console.table([numberId, nameId, cityId])
